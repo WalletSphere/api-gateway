1ced930db495a6f3d06bb5012e8ee2ab6c49f64b
Maksym Khomishchak <mkhomishchak@amelco.co.uk>
2024-02-23 22:25:34 +0000

    temporary added transactions history amount enpoint to ope APIs
    this change is a quick fix, added todo with explanation of further handling approach for given API

--- src/main/java/com/khomishchak/apigateway/validator/RouterValidator.java
+++ src/main/java/com/khomishchak/apigateway/validator/RouterValidator.java
@@ -9,8 +9,12 @@ import java.util.function.Predicate;
 @Component
 public class RouterValidator {
 
+    // TODO: update logic flow for APIs list
+    //  create an logic where we will save hash of secret keys in db where admin via separate APIs in backoffice:
+    //   /balances/transactions/history
     public static final List<String> openApiEndpoints = List.of(
-            "/auth/register", "/auth/login", "/auth/token", "/users", "/users/authenticate", "/exchangers/used-currencies"
+            "/auth/register", "/auth/login", "/auth/token", "/users", "/users/authenticate", "/exchangers/used-currencies",
+            "/balances/history/transactions/amount"
     );
 
     public Predicate<ServerHttpRequest> isPublicEndpoint =
